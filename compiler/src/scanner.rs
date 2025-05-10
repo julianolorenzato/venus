9405680338791cdc73af20f66e23cb147e8bf2c4
julianolorenzato <julianolorenzato@gmail.com>
2025-05-10 02:47:23 -0300

    feat: lex floats (f64) and integers (i32)

--- compiler/src/scanner.rs
+++ compiler/src/scanner.rs
@@ -17,7 +17,7 @@ pub(crate) enum Keyword {
     Star,
     Slash,
     Percent,
-    Equal,
+    DoubleEqual,
     BangEqual,
     Less,
     LessEqual,
@@ -27,8 +27,6 @@ pub(crate) enum Keyword {
     Or,
     And,
     If,
-    Else,
-    Loop,
     Print,
 }
 
@@ -38,7 +36,8 @@ pub enum Token {
     RightParen,
     Identifier(String),
     Keyword(Keyword),
-    Number(f64),
+    Integer(i32),
+    Float(f64),
     String(String),
     Space,
     EOF,
@@ -50,9 +49,7 @@ pub struct Scanner<I>
 where
     I: Iterator<Item = char>,
 {
-    // input: std::iter::Peekable<I>,
     input: MultiPeek<I>,
-    current_char: Option<char>,
     // Atualizar a tablela de simbolos é opcional
     // symbol_table: HashMap<String, String>,
     curr_line_idx: usize,
@@ -84,7 +81,6 @@ where
         return Scanner {
             input: multipeek(input),
             // symbol_table: HashMap::new(),
-            current_char: None,
             curr_line_idx: 0,
             curr_pos_idx: 0,
         };
@@ -92,6 +88,11 @@ where
 
     pub fn next_token(&mut self) -> Result<Token, LexicalError> {
         if let Some(c) = self.advance() {
+            // Por enqaunto só estou dando match específico em keywords que nao começam
+            // com character alfabético, e deixando a função identifier_or_keyword
+            // lidar com o resto das keywords, mas a função match_exact serviria
+            // para lidar com qualquer lexeme que tenha caractéres previamente
+            // conhecidos, isso inclui keywords.
             match c {
                 '(' => Ok(Token::LeftParen),
                 ')' => Ok(Token::RightParen),
@@ -102,6 +103,14 @@ where
                 '*' => Ok(Token::Keyword(Keyword::Star)),
                 '/' => Ok(Token::Keyword(Keyword::Slash)),
                 '%' => Ok(Token::Keyword(Keyword::Percent)),
+                '\'' => Ok(Token::Keyword(Keyword::Quote)),
+                '=' if self.match_exact('=', "==") => Ok(Token::Keyword(Keyword::DoubleEqual)),
+                '!' if self.match_exact('!', "!=") => Ok(Token::Keyword(Keyword::BangEqual)),
+                '<' if self.match_exact('<', "<=") => Ok(Token::Keyword(Keyword::LessEqual)),
+                '<' => Ok(Token::Keyword(Keyword::Less)),
+                '>' if self.match_exact('>', ">=") => Ok(Token::Keyword(Keyword::GreaterEqual)),
+                '>' => Ok(Token::Keyword(Keyword::Greater)),
+                c if c.is_ascii_digit() => self.integer_or_float(c),
                 c if c.is_ascii_alphabetic() => Ok(self.identifier_or_keyword(c)),
                 _ => Err(LexicalError::new(
                     self.curr_line_idx,
@@ -155,11 +164,56 @@ where
         if lexeme == pattern.chars().collect::<Vec<char>>() {
             if advance_count > 0 {
                 // should decrement 1 because nth(0) advances 1 position
+                //
+                // TODO! we should advance the lexer advance_count times
+                // instead of doing this:
                 self.input.nth(advance_count - 1);
             }
             true
         } else {
             false
+        }
+    }
+
+    fn integer_or_float(&mut self, first_char: char) -> Result<Token, LexicalError> {
+        let mut digit_sequence = vec![first_char];
+        let mut dot = false;
+
+        // talvez escrever uma função para abstrair essa sequencia
+        // de while let, match, break etc...
+        while let Some(c) = self.input.peek() {
+            match c {
+                c if c.is_ascii_digit() => {
+                    digit_sequence.push(*c);
+                    self.input.next();
+                }
+                c if *c == '.' && !dot => {
+                    // we just consume the dot if it is followed by
+                    // an ascii digit
+                    if let Some(forward_c) = self.input.peek() {
+                        if forward_c.is_ascii_digit() {
+                            self.advance();
+                            digit_sequence.push('.');
+                            dot = true;
+                        }
+
+                        // we dont care about double peeking here, because
+                        // its the last time we will peek before advance
+                        // the lexer in the outer function
+                    }
+                }
+                _ => {
+                    break;
+                }
+            }
+        }
+
+        if dot {
+            let float: f64 = digit_sequence.iter().collect::<String>().parse().unwrap();
+            Ok(Token::Float(float))
+        } else {
+            let integer: i32 = digit_sequence.iter().collect::<String>().parse().unwrap();
+            Ok(Token::Integer(integer))
         }
     }
 
@@ -182,9 +236,15 @@ where
         match lexeme.as_str() {
             "define" => Token::Keyword(Keyword::Define),
             "define-p" => Token::Keyword(Keyword::DefineP),
-            // "->>" => Token::Keyword(Keyword::DoubleArrow),
+            "fun" => Token::Keyword(Keyword::Fun),
             "import" => Token::Keyword(Keyword::Import),
-
+            "true" => Token::Keyword(Keyword::True),
+            "false" => Token::Keyword(Keyword::False),
+            "or" => Token::Keyword(Keyword::Or),
+            "and" => Token::Keyword(Keyword::And),
+            "not" => Token::Keyword(Keyword::Not),
+            "if" => Token::Keyword(Keyword::If),
+            "print" => Token::Keyword(Keyword::Print),
             _ => Token::Identifier(lexeme),
         }
     }
@@ -226,24 +286,6 @@ where
             }
         }
         Ok(Token::String("aaa".to_owned()))
-    }
-
-    fn check_true(&mut self) -> bool {
-        for ch in "rue".chars() {
-            if let Some(c) = self.input.peek() {
-                if *c != ch {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        }
-
-        for _ in 0..4 {
-            self.input.next();
-        }
-
-        true
     }
 }
 
